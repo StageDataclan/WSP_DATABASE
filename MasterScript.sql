f44d9923844a510da59371ca6d9ff56a6c0d7264
Djamel G <90888178+ddgaidi@users.noreply.github.com>
2025-06-18 10:34:21 +0200

    Add files via upload

---
+++ MasterScript.sql
@@ -0,0 +1,17 @@
+:r "C:\Users\djame\Documents\STAGE\SQL - PROJET 2\01_Creation_BDD_Tables.sql"
+GO
+
+:r "C:\Users\djame\Documents\STAGE\SQL - PROJET 2\02_InsertDonnees.sql"
+GO
+
+:r "C:\Users\djame\Documents\STAGE\SQL - PROJET 2\03_Fonctions.sql"
+GO
+
+:r "C:\Users\djame\Documents\STAGE\SQL - PROJET 2\04_Procédures.sql"
+GO
+
+:r "C:\Users\djame\Documents\STAGE\SQL - PROJET 2\05_Modifications_et_Index.sql"
+GO
+
+:r "C:\Users\djame\Documents\STAGE\SQL - PROJET 2\06_DimDate_et_AlimDate.sql"
+GO
